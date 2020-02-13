eb1a1d85451775ffe0a781ccac3fcf9be29dd9a5
Enrique <eabz@polispay.org>
2020-02-13 17:37:43 -0600

    Serialization funcs

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.5.0
 	github.com/joho/godotenv v1.3.0
+	github.com/olympus-protocol/ogen v0.0.0-20200205181740-655b2c2d569c
 	go.mongodb.org/mongo-driver v1.3.0
 )
--- types/mons.go
+++ types/mons.go
@@ -1,5 +1,13 @@
 package types
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"github.com/olympus-protocol/ogen/utils/serializer"
+	"io"
+)
+
 type Gender int
 type Nature int
 type Variant int
@@ -31,14 +39,72 @@ type OnChainData struct {
 type ADN struct {
 	Gender      Gender     `bson:"gender" json:"gender"`
 	BirthHeight int32      `bson:"birth_height" json:"birth_height"`
-	IV          Stats      `bson:"iv" json:"iv"`
 	Nature      Nature     `bson:"nature" json:"nature"`
 	Variant     Variant    `bson:"variant" json:"variant"`
 	Generation  Generation `bson:"generation" json:"generation"`
 	Purity      int        `bson:"purity" json:"purity"`
 	Specimen    Specimen   `bson:"specimen" json:"specimen"`
-	Other       []int      `bson:"other" json:"other"`
 	Nonce       int        `bson:"nonce" json:"nonce"`
+	Other       []int      `bson:"other" json:"other"`
+	IV          Stats      `bson:"iv" json:"iv"`
+}
+
+func (adn *ADN) GetID() (string, error) {
+	buf := bytes.NewBuffer([]byte{})
+	err := adn.Serialize(buf)
+	if err != nil {
+		return "", err
+	}
+	hash := sha256.Sum256(buf.Bytes())
+	lastBytes := hash[len(hash)-4:]
+	return hex.EncodeToString(lastBytes), nil
+}
+
+func (adn *ADN) Serialize(w io.Writer) error {
+	err := serializer.WriteElements(w, adn.Gender, adn.BirthHeight, adn.Nature, adn.Variant, adn.Generation, adn.Purity, adn.Specimen, adn.Nonce)
+	if err != nil {
+		return err
+	}
+	err = serializer.WriteVarInt(w, uint64(len(adn.Other)))
+	if err != nil {
+		return err
+	}
+	for _, other := range adn.Other {
+		err = serializer.WriteElement(w, other)
+		if err != nil {
+			return err
+		}
+	}
+	err = adn.IV.Serialize(w)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (adn *ADN) Deserialize(r io.Reader) error {
+	err := serializer.ReadElements(r, &adn.Gender, &adn.BirthHeight, &adn.Nature, &adn.Variant, &adn.Generation, &adn.Purity, &adn.Specimen, &adn.Nonce)
+	if err != nil {
+		return err
+	}
+	count, err := serializer.ReadVarInt(r)
+	if err != nil {
+		return err
+	}
+	adn.Other = make([]int, count)
+	for i := uint64(0); i < count; i++ {
+		var element int
+		err = serializer.ReadElement(r, &element)
+		if err != nil {
+			return err
+		}
+		adn.Other = append(adn.Other, element)
+	}
+	err = adn.IV.Derialize(r)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 type StatsTable map[int]Stats
@@ -49,6 +115,22 @@ type Stats struct {
 	SA int `bson:"sa" json:"sa"`
 	D  int `bson:"d" json:"d"`
 	SD int `bson:"sd" json:"sd"`
+}
+
+func (s *Stats) Serialize(w io.Writer) error {
+	err := serializer.WriteElements(w, s.H, s.A, s.SA, s.D, s.SD)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Stats) Derialize(r io.Reader) error {
+	err := serializer.ReadElements(r, &s.H, &s.A, &s.SA, &s.D, &s.SD)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 type AttacksTable map[int][]Attack
